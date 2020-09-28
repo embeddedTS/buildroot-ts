c63fbad74f9d52504347ef6a0cef8524f0d276f1
Kris Bahnsen <kris@embeddedarm.com>
2020-09-28 16:49:27 -0700

    tsimx6ul_usbprod: Merge TS-4100/TS-7553-V2 together
    Removes _usbprod_defconfig's and dir's for TS-4100 and TS-7553-V2
    and merges them in to a single script and overlay directory.

--- technologic/board/tsimx6ul-usbprod/scripts/post-image.sh
+++ technologic/board/tsimx6ul-usbprod/scripts/post-image.sh
@@ -3,13 +3,13 @@
 TEMPDIR=$(mktemp -d)
 mkdir "${TEMPDIR}"/boot/
 cp output/images/{*.dtb,zImage,rootfs.cpio.gz} "${TEMPDIR}"/boot/
-cp ../technologic/board/ts4100/usbprod-scripts/tsinit.source "${TEMPDIR}"
+cp ../technologic/board/tsimx6ul-usbprod/scripts/tsinit.source "${TEMPDIR}"
 
 echo "Generating U-Boot scripts"
 mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMPDIR}"/tsinit.scr
 mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMPDIR}"/tsinit.ub
 
-cp ../technologic/board/ts4100/usbprod-scripts/blast.sh "${TEMPDIR}/blast.sh"
-tar cjf output/images/ts4100-usb-production-rootfs.tar.bz2 -C "${TEMPDIR}" .
+cp ../technologic/board/tsimx6ul-usbprod/scripts/blast.sh "${TEMPDIR}/blast.sh"
+tar cjf output/images/tsimx6ul-usb-production-rootfs.tar.bz2 -C "${TEMPDIR}" .
 
 rm -r "$TEMPDIR"
