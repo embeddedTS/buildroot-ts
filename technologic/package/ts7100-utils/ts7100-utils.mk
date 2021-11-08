1922b7f00169e226cff842707fb706d3ae7d14ec
Mark Featherston <mark@embeddedarm.com>
2021-11-08 08:34:25 -0700

    packages: ts7100-util: Bumped to latest commit with TS-7250-V3 Fixes

--- technologic/package/ts7100-utils/ts7100-utils.mk
+++ technologic/package/ts7100-utils/ts7100-utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TS7100_UTILS_AUTORECONF = YES
-TS7100_UTILS_VERSION = 0ac22280ccef24de350dd12d40a3cb7b5c974180
+TS7100_UTILS_VERSION = 0105b4739338c58164b0e5c4535db9d4f1340bc1
 TS7100_UTILS_SITE = $(call github,embeddedarm,ts7100-utils,$(TS7100_UTILS_VERSION))
 
 $(eval $(autotools-package))
