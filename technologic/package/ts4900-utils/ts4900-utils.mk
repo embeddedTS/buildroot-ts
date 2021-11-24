bab0c5d879ed8d1d3a5ddc2d860549de83caa52f
Kris Bahnsen <kris@embeddedarm.com>
2021-11-23 17:11:47 -0800

    package/ts4900-utils: Initial commit
    Tested against upstream Buildroot TS-4900 defconfig

---
+++ technologic/package/ts4900-utils/ts4900-utils.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# ts4900-utils
+#
+################################################################################
+
+TS4900_UTILS_AUTORECONF = YES
+TS4900_UTILS_VERSION = 84a2bac6d32e9a0d40eee964dac40893f6467bd1
+TS4900_UTILS_SITE = $(call github,embeddedarm,ts4900-utils,$(TS4900_UTILS_VERSION))
+
+$(eval $(autotools-package))
