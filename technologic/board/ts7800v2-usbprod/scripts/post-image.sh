9de63503d2c77e819aa2e9eac6868499ea382b60
Kris Bahnsen <kris@embeddedTS.com>
2022-02-16 17:30:46 -0800

    post-images: Refactor script, output common names
    Sync all of the image replicator post-image scripts to be
    more generic, portable, and all have the same output files.

--- technologic/board/ts7800v2-usbprod/scripts/post-image.sh
+++ technologic/board/ts7800v2-usbprod/scripts/post-image.sh
@@ -1,21 +1,39 @@
 #!/bin/bash -e
 
+PLAT="ts7800v2"
+TAR_BASE="${PLAT}-usb-image-replicator-rootfs"
+IMG_BASE="${PLAT}-usb-image-replicator"
+TAR_PATH="${BINARIES_DIR}/${TAR_BASE}"
+IMG_PATH="${BINARIES_DIR}/${IMG_BASE}"
+DATE=$(date +"%Y%m%d")
+TAR_DATE_PATH="${TAR_PATH}-${DATE}"
+IMG_DATE_PATH="${IMG_PATH}-${DATE}"
+PLAT_SCRIPTS="${BR2_EXTERNAL_TECHNOLOGIC_PATH}/board/${PLAT}-usbprod/scripts"
+COMM_SCRIPTS="${BR2_EXTERNAL_TECHNOLOGIC_PATH}/board/usbprod-common/scripts"
+
+
 TEMPDIR=$(mktemp -d)
 mkdir "${TEMPDIR}"/boot/
-cp "${BINARIES_DIR}"/{*.dtb,zImage,rootfs.cpio.uboot} "${TEMPDIR}"/boot/
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/ts7800v2-usbprod/scripts/tsinit.source "${TEMPDIR}"
+cp "${BINARIES_DIR}"/{*.dtb,*Image,rootfs.cpio.*} "${TEMPDIR}"/boot/
+cp "${PLAT_SCRIPTS}/tsinit.source" "${TEMPDIR}"
 
 echo "Generating U-Boot scripts"
 mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMPDIR}"/tsinit.scr
 mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMPDIR}"/tsinit.ub
 
 # Scripts used for capturing/writing images
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/ts7800v2-usbprod/scripts/blast.sh "${TEMPDIR}/"
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/usbprod-common/scripts/blast_funcs.sh "${TEMPDIR}/"
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/usbprod-common/scripts/sanitize_linux_rootfs.sh "${TEMPDIR}/"
+cp "${PLAT_SCRIPTS}/blast.sh" "${TEMPDIR}/"
+cp "${COMM_SCRIPTS}/blast_funcs.sh" "${TEMPDIR}/"
+cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 
-# Create output tarball
-tar cjf "${BINARIES_DIR}"/ts7800v2-usb-image-replicator-rootfs.tar.bz2 -C "${TEMPDIR}" .
+# Remove old files before re-building output images
+rm -f ${TAR_PATH}* ${IMG_PATH}*
+
+# Create output files
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
+xz -2 "${TAR_DATE_PATH}.tar"
+ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"
 
 # Create output image
 # Based on genimage.sh from Buildroot
@@ -28,7 +46,13 @@ genimage \
   --tmppath "${GENIMAGE_TMP}" \
   --inputpath "${BINARIES_DIR}" \
   --outputpath "${BINARIES_DIR}" \
-  --config ""${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/ts7800v2-usbprod/scripts/genimage.cfg"
+  --config "${PLAT_SCRIPTS}/genimage.cfg"
+
+# Create output files
+mv "${IMG_PATH}.dd" "${IMG_DATE_PATH}.dd"
+md5sum "${IMG_DATE_PATH}.dd" > "${IMG_DATE_PATH}.dd.md5"
+xz -2 "${IMG_DATE_PATH}.dd"
+ln -sf "${IMG_BASE}-${DATE}.dd.xz" "${IMG_PATH}.dd.xz"
 
 
 rm -r "${TEMPDIR}"
--- technologic/board/tsimx6-usbprod/scripts/post-image.sh
+++ technologic/board/tsimx6-usbprod/scripts/post-image.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -e
 
+PLAT="tsimx6"
+TAR_BASE="${PLAT}-usb-image-replicator-rootfs"
+IMG_BASE="${PLAT}-usb-image-replicator"
+TAR_PATH="${BINARIES_DIR}/${TAR_BASE}"
+IMG_PATH="${BINARIES_DIR}/${IMG_BASE}"
+DATE=$(date +"%Y%m%d")
+TAR_DATE_PATH="${TAR_PATH}-${DATE}"
+IMG_DATE_PATH="${IMG_PATH}-${DATE}"
+PLAT_SCRIPTS="${BR2_EXTERNAL_TECHNOLOGIC_PATH}/board/${PLAT}-usbprod/scripts"
+COMM_SCRIPTS="${BR2_EXTERNAL_TECHNOLOGIC_PATH}/board/usbprod-common/scripts"
+
+
 TEMPDIR=$(mktemp -d)
 mkdir "${TEMPDIR}"/boot/
-cp "${BINARIES_DIR}"/{*.dtb,uImage,rootfs.cpio.uboot} "${TEMPDIR}"/boot/
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/tsimx6-usbprod/scripts/tsinit.source "${TEMPDIR}"
+cp "${BINARIES_DIR}"/{*.dtb,*Image,rootfs.cpio.*} "${TEMPDIR}"/boot/
+cp "${PLAT_SCRIPTS}/tsinit.source" "${TEMPDIR}"
 
 # Copies any non-kernel files that might have been setup there, e.g. FPGA bin
 cp "${TARGET_DIR}"/boot/* "${TEMPDIR}"/boot/
@@ -13,12 +25,18 @@ mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMP
 mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/tsinit.source "${TEMPDIR}"/tsinit.ub
 
 # Scripts used for capturing/writing images
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/tsimx6-usbprod/scripts/blast.sh "${TEMPDIR}/"
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/usbprod-common/scripts/blast_funcs.sh "${TEMPDIR}/"
-cp "${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/usbprod-common/scripts/sanitize_linux_rootfs.sh "${TEMPDIR}/"
+cp "${PLAT_SCRIPTS}/blast.sh" "${TEMPDIR}/"
+cp "${COMM_SCRIPTS}/blast_funcs.sh" "${TEMPDIR}/"
+cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 
-# Create output tarball
-tar cjf "${BINARIES_DIR}"/tsimx6-usb-image-replicator-rootfs.tar.bz2 -C "${TEMPDIR}" .
+# Remove old files before re-building output images
+rm -f ${TAR_PATH}* ${IMG_PATH}*
+
+# Create output files
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
+xz -2 "${TAR_DATE_PATH}.tar"
+ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"
 
 # Create output image
 # Based on genimage.sh from Buildroot
@@ -31,8 +49,13 @@ genimage \
   --tmppath "${GENIMAGE_TMP}" \
   --inputpath "${BINARIES_DIR}" \
   --outputpath "${BINARIES_DIR}" \
-  --config ""${BR2_EXTERNAL_TECHNOLOGIC_PATH}"/board/tsimx6-usbprod/scripts/genimage.cfg"
+  --config "${PLAT_SCRIPTS}/genimage.cfg"
+
+# Create output files
+mv "${IMG_PATH}.dd" "${IMG_DATE_PATH}.dd"
+md5sum "${IMG_DATE_PATH}.dd" > "${IMG_DATE_PATH}.dd.md5"
+xz -2 "${IMG_DATE_PATH}.dd"
+ln -sf "${IMG_BASE}-${DATE}.dd.xz" "${IMG_PATH}.dd.xz"
 
 
 rm -r "${TEMPDIR}"
-
