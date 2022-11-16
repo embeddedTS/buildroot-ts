embeddedTS/buildroot-ts

commit ca87cc440eb1d81c7a4236776277c387ee26be70

tsimx6-usbprod: Update tsimx6 to support updating the microcontroller on new boards

technologic/board/tsimx6-usbprod/scripts/blast.sh
@@ -29,9 +29,10 @@ sataimage_tar="sataimage.tar.xz sataimage.tar.bz2 sataimage.tar.gz sataimage.tar
 sataimage_img="sataimage.dd.xz sataimage.dd.bz2 sataimage.dd.gz sataimage.dd"
 sataimage="${sataimage_tar} ${sataimage_img}"
 uboot_img="u-boot.imx"
+micro_bin="micro-update.bin"
 
 # A space separated list of all potential accepted image names
-all_images="${sdimage} ${emmcimage} ${sataimage} ${uboot_img}"
+all_images="${sdimage} ${emmcimage} ${sataimage} ${uboot_img} ${micro_bin}"
 
 # Set up LED definitions, this needs to happen before blast_funcs.sh is sourced
 led_init() {
@@ -55,6 +56,14 @@ mkdir /tmp/logs
 # Rather than calling this function, the calls made here can be integrated
 # in to custom blast processes
 write_images() {
+### Check for an handle microcontroller updates.
+# This runs first since this will hard reboot if there is an update
+(
+	tsmicroupdate "/mnt/usb/${micro_bin}"
+	if [ $? != 0 ]; then
+		err_exit "Microcontroller update failed"
+	fi
+)
 
 ### Check for and handle SD images
 # Order of search preferences handled by sdimage variable

commit c757e5aba461b87634d787efc7c6dc05bf5c3230

tsimx6-usbprod: blash.sh: Check for micro_bin first
Check to see if ${micro_bin} exists before attempting to call
tsmicroupdate on it. Additionally, print line to console when
attempting and also output to log file. Log will not be visible
in most cases but that should be okay.

Fixes: ca87cc4 ("tsimx6-usbprod: Update tsimx6 to support updating the microcontroller on new boards")
Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

technologic/board/tsimx6-usbprod/scripts/blast.sh
@@ -56,14 +56,16 @@ mkdir /tmp/logs
 # Rather than calling this function, the calls made here can be integrated
 # in to custom blast processes
 write_images() {
+
 ### Check for an handle microcontroller updates.
 # This runs first since this will hard reboot if there is an update
-(
-	tsmicroupdate "/mnt/usb/${micro_bin}"
-	if [ $? != 0 ]; then
-		err_exit "Microcontroller update failed"
-	fi
-)
+if [ -e "/mnt/usb/${micro_bin}" ]; then
+	echo "========== Writing update binary to Microcontroller =========="
+	(
+		tsmicroupdate "/mnt/usb/${micro_bin}" || \
+			err_exit "Microcontroller update failed"
+	) > /tmp/logs/microcontroller-update 2>&1
+fi
 
 ### Check for and handle SD images
 # Order of search preferences handled by sdimage variable