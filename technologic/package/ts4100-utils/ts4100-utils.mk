b3cd34f1630da8252570a5deaf1e5d1eb43a4bf2
Kris Bahnsen <kris@embeddedTS.com>
2023-03-31 14:00:02 -0700

    package/ts4100-utils: add .hash, use real vers., check-package pass
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/ts4100-utils/ts4100-utils.mk
+++ technologic/package/ts4100-utils/ts4100-utils.mk
@@ -5,7 +5,9 @@
 ################################################################################
 
 TS4100_UTILS_AUTORECONF = YES
-TS4100_UTILS_VERSION = 6249d98466e6f7963f8f7ef257aa9550a00402fc
+TS4100_UTILS_VERSION = v1.0.0
 TS4100_UTILS_SITE = $(call github,embeddedTS,ts4100-utils,$(TS4100_UTILS_VERSION))
+TS4100_UTILS_LICENSE = BSD-2-Clause
+TS4100_UTILS_LICENSE_FILES = LICENSE
 
 $(eval $(autotools-package))
