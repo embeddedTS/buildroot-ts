f2f3f9c190daeaa44f60b20d54da7973f1c52e9b
Kris Bahnsen <kris@embeddedTS.com>
2023-04-26 16:57:21 -0700

    package/tssilomon-init: cleanup to pass check-package
    Includes startup script changes as well
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/tssilomon-init/tssilomon-init.mk
+++ technologic/package/tssilomon-init/tssilomon-init.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 define TSSILOMON_INIT_INSTALL_INIT_SYSV
-        $(INSTALL) -D -m 755 \
+	$(INSTALL) -D -m 755 \
 	  $(BR2_EXTERNAL_TECHNOLOGIC_PATH)/package/tssilomon-init/S99tssilomon \
-          $(TARGET_DIR)/etc/init.d/S99tssilomon
+	  $(TARGET_DIR)/etc/init.d/S99tssilomon
 endef
 
 $(eval $(generic-package))
