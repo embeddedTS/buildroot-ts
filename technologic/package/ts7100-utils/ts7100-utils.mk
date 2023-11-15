503b38206e3d8b0999db3ae3ed7ff841c9122bb0
Kris Bahnsen <kris@embeddedTS.com>
2023-11-15 13:53:55 -0700

    package/ts7100-utils: bump to v1.0.1
    Fixes #44
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/ts7100-utils/ts7100-utils.mk
+++ technologic/package/ts7100-utils/ts7100-utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TS7100_UTILS_AUTORECONF = YES
-TS7100_UTILS_VERSION = v1.0.0
+TS7100_UTILS_VERSION = v1.0.1
 TS7100_UTILS_SITE = $(call github,embeddedTS,ts7100-utils,$(TS7100_UTILS_VERSION))
 TS7100_UTILS_LICENSE = BSD-2-Clause
 TS7100_UTILS_LICENSE_FILES = LICENSE
