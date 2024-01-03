d813862faedad7e937c5e047c3edd0bfad781318
Kris Bahnsen <kris@embeddedTS.com>
2024-01-03 01:05:56 +0000

    package/ts7100z-lvgl-demo: bump to v1.0.2
    Fixes some typos
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/ts7100z-lvgl-ui-demo/ts7100z-lvgl-ui-demo.mk
+++ technologic/package/ts7100z-lvgl-ui-demo/ts7100z-lvgl-ui-demo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TS7100Z_LVGL_UI_DEMO_VERSION = v1.0.0
+TS7100Z_LVGL_UI_DEMO_VERSION = v1.0.2
 TS7100Z_LVGL_UI_DEMO_SITE = $(call github,embeddedTS,ts7100z-lvgl-ui-demo,$(TS7100Z_LVGL_UI_DEMO_VERSION))
 TS7100Z_LVGL_UI_DEMO_CONF_OPTS = -DCMAKE_C_FLAGS="-I$(STAGING_DIR)/usr/include/lvgl/" -DCMAKE_CXX_FLAGS="-I$(STAGING_DIR)/usr/include/lvgl/"
 TS7100Z_LVGL_UI_DEMO_LICENSE = BSD-2-Clause
