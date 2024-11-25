8cef2b6538d410ae586030ac447a39a3a4b9d729
Kris Bahnsen <kris@embeddedTS.com>
2024-11-25 22:12:32 +0000

    technologic/board: implement options on all platforms
    Honors IR_NO_CAPTURE_* on all platforms
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/board/tsa38x-usbprod/scripts/post-image.sh
+++ technologic/board/tsa38x-usbprod/scripts/post-image.sh
@@ -23,6 +23,7 @@ mkimage -A arm -T script -C none -n 'boot' -d "${TEMPDIR}"/boot/boot.source "${T
 
 # Scripts used for capturing/writing images
 cp "${PLAT_SCRIPTS}/blast.sh" "${TEMPDIR}/"
+cp "${COMM_SCRIPTS}/README.txt" "${TEMPDIR}/"
 cp "${COMM_SCRIPTS}/blast_funcs.sh" "${TEMPDIR}/"
 cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 
--- technologic/board/tsimx6-usbprod/scripts/blast.sh
+++ technologic/board/tsimx6-usbprod/scripts/blast.sh
@@ -223,11 +223,14 @@ fi
 
 # This is our automatic capture of disk images
 capture_images() {
-	if [ -b "${SD_DEV}" ]; then
+	if [ -b "${SD_DEV}" ] && \
+	   [ -z "${IR_NO_CAPTURE_SD}" ]; then
 		capture_img_or_tar_from_disk "${SD_DEV}" "/mnt/usb" "sd"
 	fi
 
-	if [ -b "${EMMC_DEV}" ] && [ ! -e /tmp/failed ]; then
+	if [ -b "${EMMC_DEV}" ] && \
+	   [ -z "${IR_NO_CAPTURE_EMMC}" ] && \
+	   [ ! -e /tmp/failed ]; then
 		capture_img_or_tar_from_disk "${EMMC_DEV}" "/mnt/usb" "emmc"
 	fi
 
@@ -235,12 +238,18 @@ capture_images() {
 	# and the device node is a block device.
 	readlink /sys/class/block/"$(basename ${SATA_DEV})" | grep sata >/dev/null
 	RET=${?}
-	if [ "${RET}" -eq 0 ] && [ -b "${SATA_DEV}" ] && [ ! -e /tmp/failed ]; then
+	if [ "${RET}" -eq 0 ] && \
+	   [ -b "${SATA_DEV}" ] && \
+	   [ -z "${IR_NO_CAPTURE_SATA}" ] && \
+	   [ ! -e /tmp/failed ]; then
 		capture_img_or_tar_from_disk "${SATA_DEV}" "/mnt/usb" "sata"
 	fi
 }
 
 blast_run() {
+	# Get all options that may be set
+	get_env_options "/mnt/usb/"
+
 	# Check for any one of the valid image sources, if none exist, then start
 	# the image capture process. Note that, if uboot_img exists, then no images
 	# are captured. If it does not exist, the uboot_img is not captured as this
