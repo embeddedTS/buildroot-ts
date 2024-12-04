embeddedTS/buildroot-ts

commit bfb48d1fc1c088a2aa108086b1cbbaae514ca6f1

technologic/board: update IR tar commands to follow symlinks
Fixes #45

Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

technologic/board/ts7100-usbprod/scripts/post-image.sh
@@ -31,7 +31,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/ts7250v3-usbprod/scripts/post-image.sh
@@ -31,7 +31,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/ts7800v2-usbprod/scripts/post-image.sh
@@ -30,7 +30,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsa38x-usbprod/scripts/post-image.sh
@@ -30,7 +30,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx28-usbprod/scripts/post-image.sh
@@ -41,7 +41,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx6-usbprod/scripts/post-image.sh
@@ -33,7 +33,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx6ul-usbprod/scripts/post-image.sh
@@ -30,7 +30,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/usbprod-common/scripts/blast_funcs.sh
@@ -196,7 +196,7 @@ untar_image() {
 		# Get the correct command to stream decompress the tarball
 		# and run it
 		CMD=$(get_stream_decomp "${SRC_TARBALL}")
-		${CMD} "${SRC_TARBALL}" | tar -x -C "${DST_MOUNT}" || \
+		${CMD} "${SRC_TARBALL}" | tar -xh -C "${DST_MOUNT}" || \
 		  err_exit "untar ${DST_DEV}"
 
 		sync
@@ -372,7 +372,7 @@ capture_img_or_tar_from_disk() {
 			# Copy source disk filesystem to our sparse file backed
 			# mount location. Use tar pipeline to ensure EVERY file
 			# property, permission, etc, is coped intact
-			tar -cf - -C "${TMP_SRC_DIR}"/ . | tar x -C "${TMP_DIR}" \
+			tar -chf - -C "${TMP_SRC_DIR}"/ . | tar xh -C "${TMP_DIR}" \
 			  || err_exit "copy SRC contents to TMP DST"
 
 			# Unmount the SRC disk, we should no longer need this.
@@ -408,7 +408,7 @@ capture_img_or_tar_from_disk() {
 		# as opposed to a whole disk image to save time and space.
 		if [ ${PART_CNT} -eq 1 ]; then
 			echo "Creating compressed tarball"
-			tar cf "${DST_TAR}" -C "${TMP_DIR}"/ . || \
+			tar chf "${DST_TAR}" -C "${TMP_DIR}"/ . || \
 			  err_exit "tar create ${TAR}"
 			# This two-step is needed, and repeated, because we want
 			# the .md5 file to not have any relative paths

commit 3cfa07a59c692b4874b32f02dfc803db3db471c9

technologic/board: IR tar commands only follow symlinks on extraction
The dereference option was incorrectly applied to the creation
operation as well. This will fail on some capture operations depending
on the filesystem layout and symlink configuration.

Fixes: bfb48d1 ("technologic/board: update IR tar commands to follow symlinks")

Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

technologic/board/ts7100-usbprod/scripts/post-image.sh
@@ -32,7 +32,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/ts7250v3-usbprod/scripts/post-image.sh
@@ -32,7 +32,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/ts7800v2-usbprod/scripts/post-image.sh
@@ -31,7 +31,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsa38x-usbprod/scripts/post-image.sh
@@ -31,7 +31,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx28-usbprod/scripts/post-image.sh
@@ -42,7 +42,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx6-usbprod/scripts/post-image.sh
@@ -34,7 +34,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/tsimx6ul-usbprod/scripts/post-image.sh
@@ -31,7 +31,7 @@ cp "${COMM_SCRIPTS}/sanitize_linux_rootfs.sh" "${TEMPDIR}/"
 rm -f ${TAR_PATH}* ${IMG_PATH}*
 
 # Create output files
-tar chf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
+tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
 md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"

technologic/board/usbprod-common/scripts/blast_funcs.sh
@@ -402,7 +402,7 @@ capture_img_or_tar_from_disk() {
 			# Copy source disk filesystem to our sparse file backed
 			# mount location. Use tar pipeline to ensure EVERY file
 			# property, permission, etc, is coped intact
-			tar -chf - -C "${TMP_SRC_DIR}"/ . | tar xh -C "${TMP_DIR}" \
+			tar -cf - -C "${TMP_SRC_DIR}"/ . | tar xh -C "${TMP_DIR}" \
 			  || err_exit "copy SRC contents to TMP DST"
 
 			# Unmount the SRC disk, we should no longer need this.
@@ -439,7 +439,7 @@ capture_img_or_tar_from_disk() {
 		# as opposed to a whole disk image to save time and space.
 		if [ ${PART_CNT} -eq 1 ]; then
 			echo "Creating tarball"
-			tar chf "${DST_TAR}" -C "${TMP_DIR}"/ . || \
+			tar cf "${DST_TAR}" -C "${TMP_DIR}"/ . || \
 			  err_exit "tar create ${TAR}"
 			# This two-step is needed, and repeated, because we want
 			# the .md5 file to not have any relative paths