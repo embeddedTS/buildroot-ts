af714a9810ae25e35d603c92999094a47ec17e71
Kris Bahnsen <kris@embeddedTS.com>
2025-01-06 20:08:44 +0000

    usbprod-common/scripts/post-image.sh: remove absolute path from md5
    Fixes #77
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/board/usbprod-common/scripts/post-image.sh
+++ technologic/board/usbprod-common/scripts/post-image.sh
@@ -71,11 +71,15 @@ fi
 # Remove old files before re-building output images
 rm -f "${TAR_PATH}"* "${IMG_PATH}"*
 
-# Create output files
+
+# Create tarball output files
 tar cf "${TAR_DATE_PATH}.tar" -C "${TEMPDIR}" .
-md5sum "${TAR_DATE_PATH}.tar" > "${TAR_DATE_PATH}.tar.md5"
 xz -2 "${TAR_DATE_PATH}.tar"
+md5sum "${TAR_DATE_PATH}.tar.xz" > "${TAR_DATE_PATH}.tar.xz.md5"
+# Strip the file path off of the link in the .md5 file
+sed -i -e 's/\/.*\///' "${TAR_DATE_PATH}.tar.xz.md5"
 ln -sf "${TAR_BASE}-${DATE}.tar.xz" "${TAR_PATH}.tar.xz"
+
 
 # Create output image
 # Based on genimage.sh from Buildroot
@@ -92,8 +96,10 @@ genimage \
 
 # Create output files
 mv "${IMG_PATH}.dd" "${IMG_DATE_PATH}.dd"
-md5sum "${IMG_DATE_PATH}.dd" > "${IMG_DATE_PATH}.dd.md5"
 xz -2 "${IMG_DATE_PATH}.dd"
+md5sum "${IMG_DATE_PATH}.dd.xz" > "${IMG_DATE_PATH}.dd.xz.md5"
+# Strip the file path off of the link in the .md5 file
+sed -i -e 's/\/.*\///' "${IMG_DATE_PATH}.dd.xz.md5"
 ln -sf "${IMG_BASE}-${DATE}.dd.xz" "${IMG_PATH}.dd.xz"
 
 
