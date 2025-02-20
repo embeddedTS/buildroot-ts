57144001e7f03560812d9cc1eaee9c66b749a270
Kris Bahnsen <kris@embeddedTS.com>
2025-02-20 21:59:51 +0000

    package/ts7180-utils: bump to v1.0.2
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/ts7180-utils/ts7180-utils.mk
+++ technologic/package/ts7180-utils/ts7180-utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TS7180_UTILS_AUTORECONF = YES
-TS7180_UTILS_VERSION = v1.0.0
+TS7180_UTILS_VERSION = v1.0.2
 TS7180_UTILS_SITE = $(call github,embeddedTS,ts7180-utils,$(TS7180_UTILS_VERSION))
 TS7180_UTILS_LICENSE = BSD-2-Clause
 TS7180_UTILS_LICENSE_FILES = LICENSE
