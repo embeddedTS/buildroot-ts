ddba0c5d787ffc055f3ef2844731c6ce21dfe402
Kris Bahnsen <kris@embeddedTS.com>
2025-02-20 00:07:16 +0000

    package/ts7680-utils: bump to v2.0.0
    Moves utils to use libgpiod over deprecated sysfs interface
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- technologic/package/ts7680-utils/ts7680-utils.mk
+++ technologic/package/ts7680-utils/ts7680-utils.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TS7680_UTILS_AUTORECONF = YES
-TS7680_UTILS_VERSION = v1.0.1
+TS7680_UTILS_VERSION = v2.0.0
 TS7680_UTILS_SITE = $(call github,embeddedTS,ts7680-utils,$(TS7680_UTILS_VERSION))
 TS7680_UTILS_LICENSE = BSD-2-Clause
 TS7680_UTILS_LICENSE_FILES = LICENSE
